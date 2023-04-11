2301a8af5d9ade0e96ae04de9660b46af8e068e0
strangertwo <h.volhejn@gmail.com>
2023-04-11 18:18:21 +0200

    init-cs

---
+++ init-cs/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using (StreamWriter sw = new StreamWriter("test.txt"))
+{
+    sw.WriteLine("mega, test, xd");
+}
+
+Console.WriteLine("Napsal jsem ti to souboru 'text.txt': 'mega, text, xd'");
+
+Console.ReadKey();
